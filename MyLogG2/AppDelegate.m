fae4d2b5f42e6d0484b422b9109c764995e86b51
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-12-15 18:25:33 +0530

    Final

--- MyLogG2/AppDelegate.m
+++ MyLogG2/AppDelegate.m
@@ -15,31 +15,78 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+// [START didfinishlaunching]
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSError* configureError;
+    [[GGLContext sharedInstance] configureWithError: &configureError];
+    NSAssert(!configureError, @"Error configuring Google services: %@", configureError);
+    
+    [GIDSignIn sharedInstance].delegate = self;
+    
     return YES;
 }
+// [END didfinishlaunching]
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+// [START openurl]
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    return [[GIDSignIn sharedInstance] handleURL:url
+                               sourceApplication:sourceApplication
+                                      annotation:annotation];
 }
+// [END openurl]
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+// [START signin_handler]
+- (void)signIn:(GIDSignIn *)signIn
+didSignInForUser:(GIDGoogleUser *)user
+     withError:(NSError *)error {
+    // Perform any operations on signed in user here.
+    NSString *userId = user.userID;
+    NSLog(@"%@",userId);// For client-side use only!
+    NSString *idToken = user.authentication.idToken;
+    NSLog(@"%@",idToken);
+    // Safe to send to the server
+    NSString *name = user.profile.name;
+    NSLog(@"%@",name);
+    NSString *email = user.profile.email;
+    NSLog(@"%@",email);
+       // [START_EXCLUDE]
+    
+    if ([GIDSignIn sharedInstance].currentUser.profile.hasImage == TRUE)
+    {
+        NSUInteger dimension = 30;
+        NSURL *imageURL = [user.profile imageURLWithDimension:dimension];
+        NSLog(@"%@",imageURL);
+    }
+    NSDictionary *statusText = @{@"statusText":
+                                     [NSString stringWithFormat:@"Signed in user: %@",
+                                      name]};
+    [[NSNotificationCenter defaultCenter]
+     postNotificationName:@"ToggleAuthUINotification"
+     object:nil
+     userInfo:statusText];
+    // [END_EXCLUDE]
 }
+// [END signin_handler]
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+// This callback is triggered after the disconnect call that revokes data
+// access to the user's resources has completed.
+// [START disconnect_handler]
+- (void)signIn:(GIDSignIn *)signIn
+didDisconnectWithUser:(GIDGoogleUser *)user
+     withError:(NSError *)error {
+    // Perform any operations when the user disconnects from app here.
+    // [START_EXCLUDE]
+    NSDictionary *statusText = @{@"statusText": @"Disconnected user" };
+    [[NSNotificationCenter defaultCenter]
+     postNotificationName:@"ToggleAuthUINotification"
+     object:nil
+     userInfo:statusText];
+    // [END_EXCLUDE]
 }
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
+// [END disconnect_handle
 @end
